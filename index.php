aef4e2f16e60409abb0c0ed829a3a8f896928511
Guilherme 2°B <127538519+gu1lh3rme-rodrigues@users.noreply.github.com>
2024-09-09 20:57:18 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,162 +1,89 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <title>Set_Me_Up</title>
-    <link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 
 <body>
-
+<!-- #####################################-->
+ <!-- #####################################-->
     <nav class="navbar bg-dark navbar-dark">
         <div class="container-fluid">
-            <a href="#" class="navbar-brand">
-                <img src="">
-            </a>
-            <!-- Inserido uma caixa de busca -->
-            <form action="" class="d-flex">
-                <input type="text" class="form-control m-2">
-                <button class="btn btn-danger m-2">Buscar</button>
-            </form>
-
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuCursos"
                 style="cursor:pointer" aria-expanded="true">
                 <span class="navbar-toggler-icon"></span>
-            </button>
-
+            </button> 
             <div class="navbar-collapse collapse" id="menuCursos">
                 <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" href="#">Web 1</a>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                            data-bs-target="#JanelaModal">
-                            Abrir
-                        </button>
-
-                        <div id="JanelaModal" class="modal fade">
-                            <!-- Você pode utilizar as classes: "modal-dialog modal-sm", "modal-fullscreen" e "modal-dialog-scrollable" -->
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h3 class="modal-title">Título</h3>
-                                        <button type="button" class="btn btn-close" data-bs-dismiss="modal"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum provident
-                                        dolores, repellendus
-                                        totam ipsum iste. Ut beatae vel, blanditiis accusamus quod itaque provident
-                                        molestiae,
-                                        consequatur pariatur aliquam facilis nihil veritatis recusandae sint? Vero,
-                                        dolores corporis
-                                        beatae nam totam quod tenetur error earum consequatur at atque labore qui
-                                        veritatis temporibus
-                                        iusto quia dolore consequuntur magnam dicta asperiores! Ex dolor possimus quos,
-                                        praesentium
-                                        ipsum tempore totam dignissimos quaerat ducimus. A dicta id ipsa laboriosam
-                                        eaque, quibusdam
-                                        commodi totam mollitia odio voluptates quas tenetur aut corporis officia non
-                                        sequi facere
-                                        doloremque tempora nisi pariatur eligendi! Eum adipisci numquam, quisquam ex
-                                        natus at pariatur.
-                                    </div>
-                                    <div class="modal-footer">
-                                        <a href="#">Link</a>
-                                        <button type="button" class="btn btn-danger"
-                                            data-bs-dismiss="modal">Fechar</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href="#">Web 2</a>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                            data-bs-target="#JanelaModal">
-                            Abrir
-                        </button>
-
-                        <div id="JanelaModal" class="modal fade">
-                            <!-- Você pode utilizar as classes: "modal-dialog modal-sm", "modal-fullscreen" e "modal-dialog-scrollable" -->
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h3 class="modal-title">Título</h3>
-                                        <button type="button" class="btn btn-close" data-bs-dismiss="modal"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum provident
-                                        dolores, repellendus
-                                        totam ipsum iste. Ut beatae vel, blanditiis accusamus quod itaque provident
-                                        molestiae,
-                                        consequatur pariatur aliquam facilis nihil veritatis recusandae sint? Vero,
-                                        dolores corporis
-                                        beatae nam totam quod tenetur error earum consequatur at atque labore qui
-                                        veritatis temporibus
-                                        iusto quia dolore consequuntur magnam dicta asperiores! Ex dolor possimus quos,
-                                        praesentium
-                                        ipsum tempore totam dignissimos quaerat ducimus. A dicta id ipsa laboriosam
-                                        eaque, quibusdam
-                                        commodi totam mollitia odio voluptates quas tenetur aut corporis officia non
-                                        sequi facere
-                                        doloremque tempora nisi pariatur eligendi! Eum adipisci numquam, quisquam ex
-                                        natus at pariatur.
-                                    </div>
-                                    <div class="modal-footer">
-                                        <a href="#">Link</a>
-                                        <button type="button" class="btn btn-danger"
-                                            data-bs-dismiss="modal">Fechar</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href="#">Web 3</a> <button type="button"
-                            class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#JanelaModal">
-                            Abrir
-                        </button>
-
-                        <div id="JanelaModal" class="modal fade">
-                            <!-- Você pode utilizar as classes: "modal-dialog modal-sm", "modal-fullscreen" e "modal-dialog-scrollable" -->
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h3 class="modal-title">Título</h3>
-                                        <button type="button" class="btn btn-close" data-bs-dismiss="modal"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum provident
-                                        dolores, repellendus
-                                        totam ipsum iste. Ut beatae vel, blanditiis accusamus quod itaque provident
-                                        molestiae,
-                                        consequatur pariatur aliquam facilis nihil veritatis recusandae sint? Vero,
-                                        dolores corporis
-                                        beatae nam totam quod tenetur error earum consequatur at atque labore qui
-                                        veritatis temporibus
-                                        iusto quia dolore consequuntur magnam dicta asperiores! Ex dolor possimus quos,
-                                        praesentium
-                                        ipsum tempore totam dignissimos quaerat ducimus. A dicta id ipsa laboriosam
-                                        eaque, quibusdam
-                                        commodi totam mollitia odio voluptates quas tenetur aut corporis officia non
-                                        sequi facere
-                                        doloremque tempora nisi pariatur eligendi! Eum adipisci numquam, quisquam ex
-                                        natus at pariatur.
-                                    </div>
-                                    <div class="modal-footer">
-                                        <a href="#">Link</a>
-                                        <button type="button" class="btn btn-danger"
-                                            data-bs-dismiss="modal">Fechar</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#">Web 1</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Web 2</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Web 3</a></li>
                 </ul>
             </div>
         </div>
     </nav>
+<!-- #####################################-->
+ <!-- #####################################-->
 
+    <div id="banner" class="carousel slide" data-bs-ride="carousel">
+        <!-- Galeria de imagens -->
+        <div id="imagens" class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="print1.jpg" class="d-block w-5">
+                <!-- É possível inserir textos -->
+                <div class="carousel-caption">
+                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, nemo!</h2>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="https://picsum.photos/id/901/1100/350?grayscale" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+                <img src="https://picsum.photos/id/902/1100/350?grayscale" class="d-block w-100">
+            </div>
+        </div>
+        <!-- Botões de navegação rápida em bottom -->
+        <div id="indicadores" class="carousel-indicators">
+            <button type="button" data-bs-target="#banner" data-bs-slide-to="0" class="active"></button>
+            <button type="button" data-bs-target="#banner" data-bs-slide-to="1"></button>
+            <button type="button" data-bs-target="#banner" data-bs-slide-to="2"></button>
+            <button type="button" data-bs-target="#banner" data-bs-slide-to="3"></button>
+            <button type="button" data-bs-target="#banner" data-bs-slide-to="4"></button>
+        </div>
+        <!-- Botões de navegação left/right-->
+        <button type="button" class="carousel-control-prev" data-bs-target="#banner" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button type="button" class="carousel-control-next" data-bs-target="#banner" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </button>
+    </div>
+
+<!-- #####################################-->
+<!-- #####################################-->
+
+ <!-- Card é uma estrutura semântica com header, body e footer -->
+ <div id="div-1" class="card">
+            <div class="card-header">
+                <h1 class="card-title">Lorem, ipsum.</h1>
+            </div>
+            <div class="card-body">
+                <img src="https://picsum.photos/id/1084/465/200">
+                <p class="card-text m-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, commodi dolore
+                    incidunt iure itaque culpa ipsum cumque non sequi nulla dicta soluta amet beatae? Minima dicta ad
+                    voluptatem unde commodi.</p>
+                <a href="https://www.lojaepson.com.br" target="_blank" class="btn btn-primary">Acessar</a>
+            </div>
+            <div class="card-footer">https://www.lojaepson.com.br</div>
+        </div>
+
+
+        
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
         crossorigin="anonymous"></script>
